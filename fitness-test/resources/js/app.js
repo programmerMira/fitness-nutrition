8879baf1d3a860897a55a3925279b80b9798e0fd
Elizavet-8 <businessgrazion@gmail.com>
2021-12-03 00:18:28 +0300

    fix

--- fitness-test/resources/js/app.js
+++ fitness-test/resources/js/app.js
@@ -19,7 +19,6 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 Vue.component('Formstep', require('./components/Formstep.vue').default);
 Vue.component('Buy', require('./components/Buy.vue').default);
