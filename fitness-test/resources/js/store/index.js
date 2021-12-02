e2b8bd975074e7417f54499b942230be69b55e94
Mira <programmermira@gmail.com>
2021-12-03 01:07:26 +0300

    Update index.js

--- fitness-test/resources/js/store/index.js
+++ fitness-test/resources/js/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import problemZones from './modules/problemZones'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
-        
+        problemZones,
     }
 })
