38260a1b8ac445c3922dc1230f2c34df94bda7fd
Mira <programmermira@gmail.com>
2021-12-27 02:08:33 +0300

    Revert "Update AppServiceProvider.php"
    This reverts commit 66c229ddf9892443f02b78c1eaa5d32c9270c610.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,8 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if(config('app.env') === 'production') {
-            \URL::forceScheme('https');
-        }
+        $this->app->bind('path.public', function() {
+            return base_path().'/public_html';
+        });
     }
 }
