f26b7f59401118cd140707e3037bff932dde8b64
Mira <programmermira@gmail.com>
2021-12-27 01:51:47 +0300

    added meta-tags

--- resources/views/admin/dashboard/authBase.blade.php
+++ resources/views/admin/dashboard/authBase.blade.php
@@ -10,6 +10,7 @@
 <html lang="en">
   <head>
     <base href="./">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
